e5465525af33ea95cc49585ccccbee805b8d5642
Kyle Fletcher <kfletcher87@gmail.com>
2024-03-26 22:31:46 -0500

    game start with both players

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -4,13 +4,12 @@ var decks_scene = preload("res://scenes/decks.tscn").instantiate()
 var game_scene = preload("res://scenes/game.tscn").instantiate()
 
 func _ready():
+	$StartGameButton.connect("game_ready", _on_game_ready)
 	$StartGameButton.grab_focus()
 
 func _on_decks_button_pressed():
 	get_tree().root.add_child(decks_scene)
-	#add_child(decks_scene)
 	self.visible = false
-	#self.queue_free()
 
 func _on_game_ready():
 	get_tree().root.add_child(game_scene)
--- scripts/start_game_button.gd
+++ scripts/start_game_button.gd
@@ -1,5 +1,7 @@
 extends Button
 
+signal game_ready
+
 func _on_pressed():
 	$StartGameHTTPRequest.request_completed.connect(_on_start_game_request_completed)
 	Networking.on_start_game_pressed($StartGameHTTPRequest)
@@ -13,6 +15,7 @@ func _on_start_game_request_completed(result: int, response_code: int, _headers:
 	# 200, game found, start game
 	elif (result == HTTPRequest.RESULT_SUCCESS && response_code == 200):
 		State.gameId = data.gameId
+		game_ready.emit()
 
 
 func _on_start_game_timer_timeout():
@@ -20,10 +23,9 @@ func _on_start_game_timer_timeout():
 	Networking.on_start_game_timer_timeout($StartGameRetryHTTPRequest)
 
 func _on_start_game_retry_request_completed(result: int, response_code: int, _headers: PackedStringArray, _body: PackedByteArray):
-	print("_on_start_game_retry_request_completed: ", response_code)
 	# player found, join game
 	if (result == HTTPRequest.RESULT_SUCCESS && response_code == 200):
-		pass
+		game_ready.emit()
 	# no player yet, retry
 	elif (result == HTTPRequest.RESULT_SUCCESS && response_code == 204):
 		$StartGameTimer.start()
